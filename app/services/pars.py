ce4b1ad3b2a25c855d83d5b779be3e4b6c0bedeb
TimesNewRomanCode <famas8959@gmail.com>
2025-11-17 16:01:56 +0700

    Наконец-то поправил таблицу на фото

--- app/services/pars.py
+++ app/services/pars.py
@@ -177,19 +177,20 @@ def create_group_sheets_single_column(groups, source_sheet, output_dir):
             if images:
                 img = images[0]
                 width, height = img.size
-                top_bottom_crop = 100  # одинаково сверху и снизу
-                left_right_crop = 300  # слева и справа (можете изменить)
 
-                cropped = img.crop(
-                    (
-                        left_right_crop,
-                        top_bottom_crop,
-                        width - left_right_crop,
-                        height - top_bottom_crop,
-                    )
-                )
+
+                crop_top = int(height * 0.00)  # обрезать % сверху
+                crop_bottom = int(height * 0.10)  # обрезать % снизу
+                crop_left = int(width * 0.2588)  # обрезать % слева
+                crop_right = int(width * 0.10)  # обрезать % справа
+
+                cropped = img.crop((
+                    crop_left,
+                    crop_top,
+                    width - crop_right,
+                    height - crop_bottom
+                ))
                 cropped.save(os.path.join(output_dir, f"{name}.png"), "PNG")
-
             os.remove(xlsx_path)
             os.remove(pdf_path)
 
